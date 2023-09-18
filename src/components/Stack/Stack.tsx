0142bcc88ab844cbadfe0931c953e9d0e2e56b49
daramosn <daramosn@unal.edu.co>
2023-09-18 00:36:56 -0500

    feat: add stack section

--- src/components/Stack/Stack.tsx
+++ src/components/Stack/Stack.tsx
@@ -1,9 +1,27 @@
 import './Stack.scss'
+import {
+  CssIcon,
+  HtmlIcon,
+  JavascriptIcon,
+  NextIcon,
+  ReactIcon,
+  SassIcon,
+  TypescriptIcon,
+} from '@/assets/icons/Brands'
 
 const Stack = () => {
   return (
     <section className="stack">
       <h3 className="stack__title">/Stack</h3>
+      <div className="stack__grid">
+        <NextIcon className="stack__grid-icon" />
+        <SassIcon className="stack__grid-icon" />
+        <TypescriptIcon className="stack__grid-icon" />
+        <JavascriptIcon className="stack__grid-icon" />
+        <ReactIcon className="stack__grid-icon" />
+        <HtmlIcon className="stack__grid-icon" />
+        <CssIcon className="stack__grid-icon" />
+      </div>
     </section>
   )
 }
