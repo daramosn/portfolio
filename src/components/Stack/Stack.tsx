a4e65d27b8390ee7c948e2136367accae55cc07e
daramosn <daramosn@unal.edu.co>
2023-10-15 01:11:28 -0500

    style: put grid layout in Stack section

--- src/components/Stack/Stack.tsx
+++ src/components/Stack/Stack.tsx
@@ -15,12 +15,12 @@ const Stack = () => {
       <h3 className="stack__title">/Stack</h3>
       <div className="stack__grid">
         <NextIcon className="stack__grid-icon" />
-        <SassIcon className="stack__grid-icon" />
-        <TypescriptIcon className="stack__grid-icon" />
-        <JavascriptIcon className="stack__grid-icon" />
         <ReactIcon className="stack__grid-icon" />
-        <HtmlIcon className="stack__grid-icon" />
+        <JavascriptIcon className="stack__grid-icon" />
+        <TypescriptIcon className="stack__grid-icon" />
         <CssIcon className="stack__grid-icon" />
+        <SassIcon className="stack__grid-icon" />
+        <HtmlIcon className="stack__grid-icon" />
       </div>
     </section>
   )
